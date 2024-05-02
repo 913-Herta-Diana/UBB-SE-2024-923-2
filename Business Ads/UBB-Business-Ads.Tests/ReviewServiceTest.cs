396131f224156b605221b7a83f65142039ae97f7
anitagorog <anitagorog2003@gmail.com>
2024-05-02 23:24:53 +0300

    warningtest

--- Business Ads/UBB-Business-Ads.Tests/ReviewServiceTest.cs
+++ Business Ads/UBB-Business-Ads.Tests/ReviewServiceTest.cs
@@ -1,4 +1,8 @@
-﻿namespace UBB_Business_Ads.Tests
+﻿// <copyright file="ReviewServiceTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace UBB_Business_Ads.Tests
 {
     using Backend.Services;
     using Moq;
